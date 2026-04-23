21950f2ab34c3e2c68851532e61a172e3d6c1bb7
farahk15 <faraharada15@gmail.com>
2026-04-23 22:04:37 +0300

    Updatet api.js2

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,33 +1,3 @@
-import axios from 'axios';
-
-const API = 'https://srv-d7kmh4km0tmc73aodc70.onrender.com/api'; 
-
-const api = axios.create({
-  baseURL: API,
-  headers: {
-    'Content-Type': 'application/json',
-  },
-});
-
-api.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem('token'); 
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-  },
-  (error) => Promise.reject(error)
-);
-
-api.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    if (error.response && error.response.status === 401) {
-      console.error("unauthorized, redirecting to login...");
-    }
-    return Promise.reject(error);
-  }
-);
-
-export default api;
+// utils/api.js
+const API = 'https://srv-d7kmh4km0tmc73aodc70.onrender.com/api';
+export default API;
